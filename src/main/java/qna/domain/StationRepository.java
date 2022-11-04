9a7aac0a49cd1a91b58455c51b063536ed351605
sjk5766 <wjdrnr6662@gmail.com>
2022-11-04 15:27:29 +0900

    refactor: 사용하지 않는 getter, setter 제거

--- src/main/java/qna/domain/StationRepository.java
+++
@@ -1,7 +0,0 @@
-package qna.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface StationRepository extends JpaRepository<Station, Long> {
-    Station findByName(String name); // (1)
-}
