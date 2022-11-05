c8cea339eecb73bb5828c58b5fa4ac38982f1faa
sjk5766 <wjdrnr6662@gmail.com>
2022-11-05 19:42:18 +0900

    test: repository 테스트 추가

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -1,55 +1,80 @@
 package qna.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.domain.Answer;
+import qna.domain.Question;
+import qna.domain.User;
+import qna.fixture.TestAnswerFactory;
+import qna.fixture.TestQuestionFactory;
+import qna.fixture.TestUserFactory;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static qna.domain.AnswerTest.A1;
-import static qna.domain.AnswerTest.A2;
 
 @DataJpaTest
 class AnswerRepositoryTest {
     @Autowired
     private AnswerRepository answerRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @BeforeEach
+    void clear() {
+        answerRepository.deleteAll();
+        userRepository.deleteAll();
+        questionRepository.deleteAll();
+    }
+
     @DisplayName("답변을 저장할 수 있다")
     @Test
     void save() {
-        Answer actual = answerRepository.save(A1);
+        User writer = userRepository.save(TestUserFactory.create("writer"));
+        Question question = questionRepository.save(TestQuestionFactory.create(writer));
+        Answer expect = TestAnswerFactory.create(writer, question);
+
+        Answer actual = answerRepository.save(expect);
 
         assertAll(
                 () -> assertThat(actual.getId()).isNotNull(),
-                () -> assertThat(actual.isDeleted()).isEqualTo(A1.isDeleted()),
-                () -> assertThat(actual.getContents()).isEqualTo(A1.getContents()),
-                () -> assertThat(actual.getQuestion()).isEqualTo(A1.getQuestion()),
-                () -> assertThat(actual.getWriter()).isEqualTo(A1.getWriter())
+                () -> assertThat(actual.isDeleted()).isEqualTo(expect.isDeleted()),
+                () -> assertThat(actual.getContents()).isEqualTo(expect.getContents()),
+                () -> assertThat(actual.getQuestion()).isEqualTo(expect.getQuestion()),
+                () -> assertThat(actual.getWriter()).isEqualTo(expect.getWriter())
         );
     }
 
     @DisplayName("id로 조회할 수 있다")
     @Test
     void findById() {
-        Answer actual = answerRepository.save(A1);
-        System.out.println(A1.isDeleted());
-        Answer result = answerRepository.findByIdAndDeletedFalse(actual.getId()).get();
-        System.out.println(result);
-        assertThat(actual == result).isTrue();
+        User writer = userRepository.save(TestUserFactory.create("writer"));
+        Question question = questionRepository.save(TestQuestionFactory.create(writer));
+        Answer expect = answerRepository.save(TestAnswerFactory.create(writer, question));
+
+        Answer result = answerRepository.findByIdAndDeletedFalse(expect.getId()).get();
+
+        assertThat(result == expect).isTrue();
     }
 
     @DisplayName("답변이 삭제되었을 경우, findByIdAndDeletedFalse 함수로 조회할 수 없다")
     @Test
     void findDeletedById() {
-        Answer actual = answerRepository.save(A2);
-        actual.setDeleted(true);
+        User writer = userRepository.save(TestUserFactory.create("writer"));
+        Question question = questionRepository.save(TestQuestionFactory.create(writer));
+        Answer answer = answerRepository.save(TestAnswerFactory.create(writer, question));
+        answer.setDeleted(true);
 
-        Optional<Answer> result = answerRepository.findByIdAndDeletedFalse(actual.getId());
+        Optional<Answer> result = answerRepository.findByIdAndDeletedFalse(answer.getId());
 
         assertThat(result).isNotPresent();
     }
@@ -57,20 +82,24 @@ class AnswerRepositoryTest {
     @DisplayName("질문의 id로 조회할 수 있다")
     @Test
     void findByQuestionId() {
-        Answer actual = answerRepository.save(A1);
+        User writer = userRepository.save(TestUserFactory.create("writer"));
+        Question question = questionRepository.save(TestQuestionFactory.create(writer));
+        Answer expect = answerRepository.save(TestAnswerFactory.create(writer, question));
 
-        Answer result = answerRepository.findByQuestionIdAndDeletedFalse(actual.getQuestion().getId()).get(0);
+        Answer result = answerRepository.findByQuestionIdAndDeletedFalse(expect.getQuestion().getId()).get(0);
 
-        assertThat(actual == result).isTrue();
+        assertThat(expect == result).isTrue();
     }
 
     @DisplayName("답변이 삭제되었을 경우, findByQuestionIdAndDeletedFalse 함수로 조회할 수 없다")
     @Test
     void findDeletedByQuestionId() {
-        Answer actual = answerRepository.save(A2);
-        actual.setDeleted(true);
+        User writer = userRepository.save(TestUserFactory.create("writer"));
+        Question question = questionRepository.save(TestQuestionFactory.create(writer));
+        Answer answer = answerRepository.save(TestAnswerFactory.create(writer, question));
+        answer.setDeleted(true);
 
-        List<Answer> result = answerRepository.findByQuestionIdAndDeletedFalse(actual.getQuestion().getId());
+        List<Answer> result = answerRepository.findByQuestionIdAndDeletedFalse(answer.getQuestion().getId());
 
         assertThat(result).hasSize(0);
     }
--- src/test/java/qna/repository/UserRepositoryTest.java
+++ src/test/java/qna/repository/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.domain.User;
+import qna.fixture.TestUserFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -18,24 +19,25 @@ class UserRepositoryTest {
     @DisplayName("사용자를 저장할 수 있다")
     @Test
     void save() {
-        User actual = userRepository.save(JAVAJIGI);
+        User expect = TestUserFactory.create("서정국");
+        User result = userRepository.save(expect);
 
         assertAll(
-                () -> assertThat(actual.getId()).isNotNull(),
-                () -> assertThat(actual.getUserId()).isEqualTo(JAVAJIGI.getUserId()),
-                () -> assertThat(actual.getName()).isEqualTo(JAVAJIGI.getName()),
-                () -> assertThat(actual.getPassword()).isEqualTo(JAVAJIGI.getPassword()),
-                () -> assertThat(actual.getEmail()).isEqualTo(JAVAJIGI.getEmail())
+                () -> assertThat(result.getId()).isNotNull(),
+                () -> assertThat(result.getUserId()).isEqualTo(expect.getUserId()),
+                () -> assertThat(result.getName()).isEqualTo(expect.getName()),
+                () -> assertThat(result.getPassword()).isEqualTo(expect.getPassword()),
+                () -> assertThat(result.getEmail()).isEqualTo(expect.getEmail())
         );
     }
 
     @DisplayName("사용자 계정으로 조회할 수 있다")
     @Test
     void findByUserId() {
-        User actual = userRepository.save(JAVAJIGI);
-        
-        User result = userRepository.findByUserId(actual.getUserId()).get();
+        User expect = userRepository.save(TestUserFactory.create("서정국"));
 
-        assertThat(actual == result).isTrue();
+        User result = userRepository.findByUserId(expect.getUserId()).get();
+
+        assertThat(expect == result).isTrue();
     }
 }
